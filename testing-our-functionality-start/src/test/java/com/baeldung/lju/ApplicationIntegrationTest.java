df43d3857e19642cee598b1cdba93962a0112e9a
Khandker Tanvir <khandker.tanvir.hasan@gmail.com>
2025-11-18 14:21:44 +0600

    module 1 lessson 5 day 2 commit 1

---
+++ testing-our-functionality-start/src/test/java/com/baeldung/lju/ApplicationIntegrationTest.java
@@ -0,0 +1,10 @@
+package com.baeldung.lju;
+
+import org.junit.jupiter.api.Test;
+
+public class ApplicationIntegrationTest {
+    @Test
+    void mainAppMethodIntegrationTest() {
+        LjuApp.main(new String[] {});
+    }
+}
